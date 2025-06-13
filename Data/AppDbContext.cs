2d0c0ac7a800851044604015585ead58ed6c91b5
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-06-13 17:38:23 -0300

    Integrate Entity Framework Core with SQL Server
    Added EF Core packages and configured AppDbContext for SQL Server.
    Created entity models (AutorModel, LivroModel) and defined their
    relationships. Added database migrations to create schema for
    Autores and Livros tables. Configured connection string in
    appsettings.json and updated Program.cs to set up services and
    database connection. Updated project structure and dependencies
    in WebApi-estudo-Biblioteca.csproj.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_estudo_Biblioteca.Models;
+
+namespace WebApi_estudo_Biblioteca.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AutorModel> Autores { get; set; }
+        public DbSet<LivroModel> Livros { get; set; }
+    }
+}
